1c3d2304689e53ec38eca46835b8ba3d509adf58
admin <hassankhan847754@gmail.com>
2023-12-15 10:47:58 +0500

    Login with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
-import 'package:flutter/material.dart';
+import 'dart:js';
 
-void main() {
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
+
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -14,15 +24,27 @@ class MyApp extends StatelessWidget {
   }
 }
 class MyHome extends StatefulWidget {
-  const MyHome({super.key});
-
   @override
   State<MyHome> createState() => _MyHomeState();
 }
 
 class _MyHomeState extends State<MyHome> {
+
+  TextEditingController userEmail = TextEditingController();
+  TextEditingController userpassword = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+
+    void login()async{
+      try{
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+            email: userEmail.text, password: userpassword.text);
+      } on FirebaseAuthException catch(error){
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("${error.code.toString()}")));
+      }
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text("My Login"),
@@ -30,19 +52,18 @@ class _MyHomeState extends State<MyHome> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-
             Container(
                margin: EdgeInsets.symmetric(horizontal: 10,vertical: 10),
                 width:double.infinity,
-
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(15)
                   ),
                   child: TextFormField(
-                    decoration: InputDecoration(
+                      controller: userEmail,
+                      decoration: InputDecoration(
                       prefixIcon: Icon(Icons.person),
-                      hintText: "john ",
-                      label: Text("Enter Your Name"),
+                      hintText: "john@gmail.com",
+                      label: Text("Enter Your Email"),
                       border:OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
                     ),
                   ),
@@ -55,6 +76,8 @@ class _MyHomeState extends State<MyHome> {
                 borderRadius: BorderRadius.circular(15)
             ),
             child: TextFormField(
+              controller: userpassword,
+
               decoration: InputDecoration(
                 prefixIcon: Icon(Icons.email_outlined),
 
@@ -64,7 +87,11 @@ class _MyHomeState extends State<MyHome> {
               ),
             ),
           ),
-          ElevatedButton(onPressed: (){}, child: Text("Login"))
+          ElevatedButton(onPressed: (){
+            // print(userEmail);
+            // print(userpassword);
+            login();
+          }, child: Text("Login"))
 
 
         ],
